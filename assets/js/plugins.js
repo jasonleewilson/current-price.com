2e895b7bdb954374826d933ab32b7c39d2a61690
jasonleewilson <info@jasonleewilson.com>
2018-07-03 14:46:38 -0700

    update nav w/ active class

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -10,3 +10,8 @@ $( ".btn-night" ).click(function() {
 
 $('.selectpicker').val('100');
 $('.selectpicker').selectpicker('render');
+
+$( '#bs-example-navbar-collapse-1 .navbar-nav a' ).on( 'click', function () {
+	$( '#bs-example-navbar-collapse-1 .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+	$( this ).parent( 'li' ).addClass( 'active' );
+});
